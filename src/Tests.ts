0565f1efc096030274a7a18ea15d1585778a3072
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-10-07 20:12:19 -0500

    Modified Vector name

--- src/Tests.ts
+++ src/Tests.ts
@@ -1,10 +1,10 @@
-import { vector } from "./utils/data-structures/vector";
+import { Vector } from "./utils/data-structures/Vector";
 import { Component } from './types';
 
 // Función para generar una letra aleatoria entre M, N, S, T e I
 function generarLetraAleatoria(): string {
 
-    const myVector = new vector<string>();
+    const myVector = new Vector<string>();
     myVector.push_back('M');
     myVector.push_back('N');
     myVector.push_back('S');
@@ -14,10 +14,10 @@ function generarLetraAleatoria(): string {
     return myVector.at(indiceAleatorio);
 }
 
-// Función principal que utiliza la función anterior y almacena los números en un vector
+// Función principal que utiliza la función anterior y almacena los números en un Vector
 function Test() {
     const cantidadNumeros = 10000; // Puedes cambiar esto según cuántos números quieras generar
-    const myVector = new vector<string>();
+    const myVector = new Vector<string>();
 
     for (let i = 0; i < cantidadNumeros; i++) {
         const numeroAleatorio = generarLetraAleatoria();
@@ -28,15 +28,15 @@ function Test() {
     console.log(myVector);
 
     const componentes: Component[] = ["math", "natural sciences", "social sciences", "text analysis", "image analysis"];
-    const vectorAleatorio: Component[] = [];
+    const VectorAleatorio: Component[] = [];
 
     for (let i = 0; i < 10000; i++) {
         const indiceAleatorio = Math.floor(Math.random() * componentes.length);
         const componenteAleatorio = componentes[indiceAleatorio];
-        vectorAleatorio.push(componenteAleatorio);
+        VectorAleatorio.push(componenteAleatorio);
     }
     console.log("Vector de Componentes aleatorios:");
-    console.log(vectorAleatorio);
+    console.log(VectorAleatorio);
 
 
 }
