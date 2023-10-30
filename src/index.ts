733e37ddf8e69e127dd0a7e85e5fe0da73d3b0dd
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-10-30 18:36:20 -0500

    Modified server file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 export {};
 
-const express = require('express');
-const mongoose = require('mongoose');
-const appRoutes = require('./routes/routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const appRoutes = require("./routes/routes");
 
 // Constansts
 
@@ -17,19 +17,19 @@ const app = express();
 // Middleware
 
 app.use(express.json()); // middleware that converts req.body to json
-app.use('/api/', appRoutes);
-
+app.use("/api/", appRoutes);
 
 // First route
-mongoose.connect(process.env.MONGO_URI)
-    .then(() =>{
-        // Only listen when we're connected to the database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Only listen when we're connected to the database
 
-        // Listen for requests
-        app.listen(PORT, ()=>{
-            console.log('Connected to DB and listening on port',  PORT )
-        });
-    })
-    .catch((error: Error) =>{
-        console.log(error);
-    })
+    // Listen for requests
+    app.listen(PORT, () => {
+      console.log("Connected to DB and listening on port", PORT);
+    });
+  })
+  .catch((error: Error) => {
+    console.log(error);
+  });
