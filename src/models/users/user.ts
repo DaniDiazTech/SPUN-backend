b4dabeadc32b377825d44e1a764fd794f0450f8c
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-10-30 21:28:42 -0500

    Added models

---
+++ src/models/users/user.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: false,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique:true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("User", userSchema);
