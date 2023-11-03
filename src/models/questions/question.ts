8f39be96f280a3d112f9b0437dea78bfa813e1f8
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-11-02 21:29:44 -0500

    Removed unnecesary field in question

--- src/models/questions/question.ts
+++ src/models/questions/question.ts
@@ -2,11 +2,6 @@ import { Schema, model } from "mongoose";
 
 const questionSchema = new Schema(
   {
-    subject: {
-      type: Schema.Types.ObjectId,
-      ref: "Subject",
-      required: true,
-    },
     text: {
       type: String,
       required: true,
